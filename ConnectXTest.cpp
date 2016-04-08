c7744eed445e90d9e70d9b72a0d49dc13b951d0a
kbhardwaj006956665 <kbhardwaj.mail.csuchico.edu>
2016-04-08 09:59:26 -0700

    Quiz3

--- ConnectXTest.cpp
+++ ConnectXTest.cpp
@@ -19,3 +19,98 @@ TEST(ConnectXTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
 }
+
+TEST(ConnectXTest, ConscCheck0)
+{
+	ConnectX obj(-1,2,-3);
+}
+
+TEST(ConnectXTest, ConscCheck)
+{
+	ConnectX obj(10,12,13);
+}
+
+TEST(ConnectXTest, ConscCheck1)
+{
+	ConnectX obj(1,-20,3);
+}
+
+TEST(ConnectXTest, ConscCheck2)
+{
+	ConnectX obj(1,20,-30);
+}
+
+TEST(ConnectXTest, ConscCheck3)
+{
+	ConnectX obj(-0.1,2,3);
+}
+
+
+TEST(ConnectXTest, placePiece2)
+{
+	ConnectX obj;
+	obj.placePiece(5);
+	obj.placePiece(5);
+	obj.showBoard();
+}
+
+TEST(ConnectXTest, placePiece3)
+{
+	ConnectX obj;
+	obj.placePiece(2);
+	obj.placePiece(3);
+	obj.placePiece(1);
+	obj.placePiece(4);
+	obj.placePiece(6);
+	obj.placePiece(112);
+	obj.showBoard();
+}
+
+TEST(ConnectXTest, CheckATWithNegavtive1)
+{
+	ConnectX obj;
+	ASSERT_EQ(-1, obj.at(1,-3));
+}
+
+TEST(ConnectXTest, CheckATWithBothNegavtive)
+{
+	ConnectX obj;	
+	ASSERT_EQ(-1, obj.at(-11,-3));
+}
+
+TEST(ConnectXTest, CheckATWith1stNegavtive)
+{
+	ConnectX obj;	
+	ASSERT_FALSE(obj.at(-1,3));
+}
+
+TEST(ConnectXTest, CheckATWithBothPositive)
+{
+	ConnectX obj;
+	bool ret;
+	ret = obj.at(10,13);
+	ASSERT_TRUE(ret);
+}
+
+TEST(ConnectXTest, CheckATOutofBound)
+{
+	ConnectX obj;
+	bool ret;
+	ret = obj.at(22231, 12121);
+	ASSERT_TRUE(ret);
+}
+
+TEST(ConnectXTest, placePieceNegativeValue)
+{
+	ConnectX obj;
+	obj.placePiece(-3);
+	obj.showBoard();
+}
+
+
+TEST(ConnectXTest, placePieceOutOfBound)
+{
+	ConnectX obj;
+	obj.placePiece(10);
+	obj.showBoard();
+}
---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+
+Error:
+
+1. The return type of inBounds() only depend on the value of the second argument. 
+2. inBounds(), does not do what it is supposed to do. It checks the value for both arguments but its return type is based only on the second argument. 
